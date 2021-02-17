4c2e8e77df87c22c8f9f63a10eab04ddbc983d1e
davidmorasalazar <davidmsalazar99@gmail.com>
2021-02-17 03:01:56 +0000

    my commit 14

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -9,7 +9,28 @@ export const Navbar = () => {
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<div className="btn-group btn-primary" role="group" aria-label="Button group with nested dropdown">
+						<div className="btn-group btn-primary" role="group">
+							<button
+								id="btnGroupDrop1"
+								type="button"
+								className="btn btn-primary dropdown-toggle"
+								data-toggle="dropdown"
+								aria-haspopup="true"
+								aria-expanded="false">
+								Favorites
+								<span className="badge badge-secondary ml-1">4</span>
+							</button>
+							<div className="dropdown-menu" aria-labelledby="btnGroupDrop1">
+								<a name="txtanchor" className="dropdown-content" href="#">
+									Dropdown link
+								</a>
+								<a name="txtanchor" className="dropdown-content" href="#">
+									Dropdown link
+								</a>
+							</div>
+						</div>
+					</div>
 				</Link>
 			</div>
 		</nav>
